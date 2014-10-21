31e5ae493fa6ec132587e91830b529fc434afa0d
Lynn Hurley <lynn.dylan.hurley@gmail.com>
2014-10-21 17:04:05 -0500

    [wip] travis

--- gulpfile.js
+++ gulpfile.js
@@ -554,7 +554,7 @@ gulp.task('test:e2e', function(cb) {
   console.log('@-->test:e2e')
   seq(
     'build-dev',
-    'start-sauce-connect',
+    //'start-sauce-connect',
     'start-e2e-server',
     'verify-e2e-server',
     'run-e2e-tests',
